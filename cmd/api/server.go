450c7825f969b7c5a34a12717a912f6476a955b5
ahadiwasti <ahadiwasti@gmail.com>
2023-05-22 16:39:32 +0400

    form shell

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,55 +2,48 @@ package api
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"strings"
-	"time"
 
-	"./pkg/api/cache"
-	"./pkg/api/dao"
-	"./pkg/api/domain/account/ldap"
-	"./pkg/api/domain/perm"
-	"./pkg/api/log"
-	"./pkg/api/logger"
-	"./pkg/api/middleware"
-	"./pkg/api/router"
+	// "./pkg/api/cache"
+	// "./pkg/api/dao"
+	// "./pkg/api/domain/account/ldap"
+	// "./pkg/api/domain/perm"
+	// "./pkg/api/log"
+	// "./pkg/api/logger"
+	// "./pkg/api/middleware"
+	// "./pkg/api/router"
+
 	"github.com/gin-gonic/gin"
-	"github.com/rs/zerolog"
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var (
-	config   string
-	port     string
-	loglevel uint8
-	cors     bool
-	cluster  bool
-	//StartCmd : set up restful api server
-	StartCmd = &cobra.Command{
-		Use:     "server",
-		Short:   "Starting ahadiwasti development server",
-		Example: "using config file from config/in-local.yaml",
-		PreRun: func(cmd *cobra.Command, args []string) {
-			usage()
-			setup()
-		},
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return run()
-		},
-	}
-)
+// var (
+// 	config   string
+// 	port     string
+// 	loglevel uint8
+// 	cors     bool
+// 	cluster  bool
+// 	//StartCmd : set up restful api server
+// 	StartCmd = &cobra.Command{
+// 		Use:     "server",
+// 		Short:   "Starting ahadiwasti development server",
+// 		Example: "using config file from config/in-local.yaml",
+// 		PreRun: func(cmd *cobra.Command, args []string) {
+// 			usage()
+// 			setup()
+// 		},
+// 		RunE: func(cmd *cobra.Command, args []string) error {
+// 			return run()
+// 		},
+// 	}
+// )
 
-func init() {
-	http.DefaultClient.Timeout = time.Second * 2
-	StartCmd.PersistentFlags().StringVarP(&config, "config", "c", "./config/in-local.yaml", "Start server with provided configuration file")
-	StartCmd.PersistentFlags().StringVarP(&port, "port", "p", "8081", "Tcp port server listening on")
-	StartCmd.PersistentFlags().Uint8VarP(&loglevel, "loglevel", "l", 0, "Log level")
-	StartCmd.PersistentFlags().BoolVarP(&cors, "cors", "x", true, "Enable cors headers")
-	StartCmd.PersistentFlags().BoolVarP(&cluster, "cluster", "s", false, "cluster-alone mode or distributed mod")
-}
+// func init() {
+// 	http.DefaultClient.Timeout = time.Second * 2
+// 	StartCmd.PersistentFlags().StringVarP(&config, "config", "c", "./config/in-local.yaml", "Start server with provided configuration file")
+// 	StartCmd.PersistentFlags().StringVarP(&port, "port", "p", "8081", "Tcp port server listening on")
+// 	StartCmd.PersistentFlags().Uint8VarP(&loglevel, "loglevel", "l", 0, "Log level")
+// 	StartCmd.PersistentFlags().BoolVarP(&cors, "cors", "x", true, "Enable cors headers")
+// 	StartCmd.PersistentFlags().BoolVarP(&cluster, "cluster", "s", false, "cluster-alone mode or distributed mod")
+// }
 
 func usage() {
 	usageStr := `Developed By ahadiwasti.com`
@@ -59,55 +52,60 @@ func usage() {
 }
 
 func setup() {
-	//1.Set up log level
-	zerolog.SetGlobalLevel(zerolog.Level(loglevel))
-	//2.Set up configuration
-	viper.SetConfigFile(config)
-	content, err := ioutil.ReadFile(config)
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Read config file fail: %s", err.Error()))
-	}
-	//Replace environment variables
-	err = viper.ReadConfig(strings.NewReader(os.ExpandEnv(string(content))))
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Parse config file fail: %s", err.Error()))
-	}
-	//3.Set up run mode
-	mode := viper.GetString("mode")
-	configs := logger.Configuration{
-		EnableConsole:     viper.GetBool("EnableConsole"),
-		ConsoleLevel:      logger.Debug,
-		ConsoleJSONFormat: true,
-		EnableFile:        viper.GetBool("EnableFile"),
-		FileLevel:         logger.Infos,
-		FileJSONFormat:    true,
-		FileLocation:      viper.GetString("FileLocation"),
-		MaxSize:           viper.GetInt("MaxSize"),
-		Compress:          viper.GetBool("Compress"),
-		MaxAge:            viper.GetInt("MaxAge"),
-	}
-	errlo := logger.NewLogger(configs, logger.InstanceZapLogger)
-	if errlo != nil {
-		fmt.Println("Could not instantiate log", errlo.Error())
-	}
-	logger.Infof("%s", "File Logger Started")
-	gin.SetMode(mode)
+	// //1.Set up log level
+	// zerolog.SetGlobalLevel(zerolog.Level(loglevel))
+	// //2.Set up configuration
+	// viper.SetConfigFile(config)
+	// content, err := ioutil.ReadFile(config)
+	// if err != nil {
+	// 	log.Fatal(fmt.Sprintf("Read config file fail: %s", err.Error()))
+	// }
+	// //Replace environment variables
+	// err = viper.ReadConfig(strings.NewReader(os.ExpandEnv(string(content))))
+	// if err != nil {
+	// 	log.Fatal(fmt.Sprintf("Parse config file fail: %s", err.Error()))
+	// }
+	// //3.Set up run mode
+	// mode := viper.GetString("mode")
+	// configs := logger.Configuration{
+	// 	EnableConsole:     viper.GetBool("EnableConsole"),
+	// 	ConsoleLevel:      logger.Debug,
+	// 	ConsoleJSONFormat: true,
+	// 	EnableFile:        viper.GetBool("EnableFile"),
+	// 	FileLevel:         logger.Infos,
+	// 	FileJSONFormat:    true,
+	// 	FileLocation:      viper.GetString("FileLocation"),
+	// 	MaxSize:           viper.GetInt("MaxSize"),
+	// 	Compress:          viper.GetBool("Compress"),
+	// 	MaxAge:            viper.GetInt("MaxAge"),
+	// }
+	// errlo := logger.NewLogger(configs, logger.InstanceZapLogger)
+	// if errlo != nil {
+	// 	fmt.Println("Could not instantiate log", errlo.Error())
+	// }
+	// logger.Infof("%s", "File Logger Started")
+	gin.SetMode("DEV")
 	//4.Set up database connection
-	dao.Setup()
-	//5.Set up cache
-	cache.SetUp()
-	//6.Set up ldap
-	ldap.Setup()
-	//7.Set up permission handler
-	perm.SetUp(cluster)
-	//9.Initialize language
-	middleware.InitLang()
+	// dao.Setup()
+	// //5.Set up cache
+	// cache.SetUp()
+	// //6.Set up ldap
+	// ldap.Setup()
+	// //7.Set up permission handler
+	// perm.SetUp(cluster)
+	// //9.Initialize language
+	// middleware.InitLang()
 }
 
 func run() error {
 	// engine := gin.Default()
 	engine := gin.New()
 	engine.Use(gin.Recovery())
-	router.SetUp(engine, cors)
-	return engine.Run(":" + port)
+	engine.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	// router.SetUp(engine, true)
+	return engine.Run(":" + "3004")
 }
--- pkg/api/router/router.go
+++ pkg/api/router/router.go
@@ -3,14 +3,12 @@ package router
 import (
 	_ "./docs"
 	"./pkg/api/controllers"
-	"./pkg/api/domain/account"
 	"./pkg/api/middleware"
 	"github.com/appleboy/gin-jwt/v2"
 	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 var jwtAuth *jwt.GinJWTMiddleware
@@ -24,7 +22,6 @@ func SetUp(e *gin.Engine, cors bool) {
 		e.Use(middleware.Cors())
 	}
 	e.Use(middleware.SetLangVer())
-	e.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	e.GET("/healthcheck", controllers.Healthy)
 	e.Static("/file", "./data/images/") // 添加图片资源路径
 	//version fragment
@@ -32,135 +29,6 @@ func SetUp(e *gin.Engine, cors bool) {
 	//install
 	InstallController := controllers.InstallController{}
 	v1.POST("/install", InstallController.Install)
-	v1.GET("/install/isLock", InstallController.IsLock)
-
-	jwtAuth = middleware.JwtAuth(account.LoginStandard)
-	//authController := &controllers.AuthController{}
-	//api handlers
-	userController := &controllers.UserController{}
-	accountController := &controllers.AccountController{}
-
-	v1.POST("/account/sms-send-check", accountController.SmsSendCheck)
-	v1.POST("/account/sms-send-code", accountController.SmsSendCode)
-
-	v1.POST("/users/login", jwtAuth.LoginHandler)
-	v1.POST("/users/login/refresh", jwtAuth.RefreshHandler)
-	// oauth login
-	jwtAuths = middleware.JwtAuth(account.LoginOAuth)
-	v1.POST("/users/login/oauth", jwtAuths.LoginHandler)
-	// ldap login
-	jwtAuths = middleware.JwtAuth(account.LoginLdap)
-	v1.POST("/users/login/ldap", jwtAuths.LoginHandler)
-
-	v1.Use(jwtAuths.MiddlewareFunc(), middleware.JwtPrepare)
-	v1.Use(jwtAuth.MiddlewareFunc(), middleware.JwtPrepare)
-	v1.Use(middleware.AccessLog)
-
-	//sdk related
-	v1.GET("/user/perm/list", userController.GetDomainPermissions)
-	v1.POST("/user/perm/check", userController.DomainPermCheck)
-	v1.GET("/user/menu", userController.GetDomainMenus)
-
-	//account - login user
-	v1.GET("/account/info", accountController.Info)
-	v1.GET("/account/idle", accountController.CheckIdle)
-	v1.GET("/account/require-change-pwd", accountController.CheckIfNeedToChangePwd)
-	v1.GET("/account/permissions", accountController.GetPermissionsWithMenu)
-	v1.PUT("/account/password", accountController.EditPassword)
-	v1.GET("/account/domains", accountController.GetDomains)
-	v1.POST("/account/bind-google-2fa-code", accountController.BindGoogle2faCode)
-	v1.GET("/account/third-bind/from/:from", accountController.ThirdBind)
-	v1.POST("/account/third-unbind", accountController.ThirdUnbind)
-	v1.GET("/account/thirds", accountController.ThirdList)
-	v1.POST("/account/send-verify-email", accountController.SendVerifymail)
-	v1.GET("/account/active", accountController.EmailVerify)
-	v1.GET("/account/security", accountController.AccountInfo)
-	v1.POST("/account/bindcode", accountController.BindGoogle2faCode)
-	v1.POST("/account/close2fa", accountController.Close2fa)
-	v1.POST("/account/check-google-2fa-code", accountController.CheckGoogle2faCode)
-	v1.GET("/account/find-code-open", accountController.FindCodeOpen) // is check google 2fa code
-	v1.POST("/account/ldap-adduser", accountController.LdapAddUser)   // add ldap user
-	v1.POST("/account/upload-avatar", accountController.UploadAvatar)
-
-	// v1.Use(middleware.PermCheck)
-	//user
-	v1.GET("/users", userController.List)
-	v1.GET("/users/:id", userController.Get)
-	//v1.GET("/users/:id/permissions", userController.GetUserPermissionsWithMenu)
-	v1.POST("/userCreate", userController.Create)
-	v1.PUT("/users/:id", userController.Edit)
-	v1.PUT("/users/:id/status", userController.EditStatus)
-	v1.PUT("/users/:id/password", userController.ResetPassword)
-	v1.GET("/users/:id/roles", userController.Roles)
-
-	v1.DELETE("/users/:id", userController.Delete)
-	v1.POST("/users/department/move", userController.UpdateDepartment)
-
-	//role
-	roleController := &controllers.RoleController{}
-	v1.GET("/roles", roleController.List)
-	v1.GET("/roles/:id", roleController.Get)
-	v1.POST("/roles", roleController.Create)
-	v1.PUT("/roles/:id", roleController.Edit)
-	v1.DELETE("/roles/:id", roleController.Delete)
-	// //device
-	// deviceController := &controllers.DeviceController{}
-	// v1.GET("/devices", deviceController.List)
-	// v1.POST("/devices", deviceController.Create)
-	// v1.PUT("/devices/:id", deviceController.Edit)
-	// v1.DELETE("/devices/:id", deviceController.Delete)
-	//menu
-	menuController := &controllers.MenuController{}
-	v1.GET("/menus", menuController.List)
-	v1.GET("/menus/:id", menuController.Get)
-	v1.POST("/menus", menuController.Create)
-	v1.PUT("/menus/:id", menuController.Edit)
-	v1.DELETE("/menus/:id", menuController.Delete)
-	//domain
-	domainController := &controllers.DomainController{}
-	v1.GET("/domains", domainController.List)
-	v1.GET("/domains/:id", domainController.Get)
-	v1.POST("/domains", domainController.Create)
-	v1.PUT("/domains/:id", domainController.Edit)
-	v1.DELETE("/domains/:id", domainController.Delete)
-
-	//dept
-	deptController := &controllers.DeptController{}
-	v1.GET("/depts", deptController.List)
-	v1.GET("/depts/:id", deptController.Get)
-	v1.POST("/depts", deptController.Create)
-	v1.PUT("/depts/:id", deptController.Edit)
-	v1.DELETE("/depts/:id", deptController.Delete)
-	v1.POST("/depts/:id/check-no-member", deptController.CheckNoMember)
-
-	// data permission
-	dataPermController := &controllers.DatePermController{}
-	v1.GET("/datas", dataPermController.List)
-	v1.GET("/datas/:id", dataPermController.Get)
-	v1.POST("/datas", dataPermController.Create)
-	v1.PUT("/datas/:id", dataPermController.Edit)
-	v1.DELETE("/datas/:id", dataPermController.Delete)
-
-	logController := controllers.LogController{}
-	//login log
-	v1.GET("/log/logins", logController.LoginLogLists)
-	v1.GET("/log/login/:id", logController.LoginLogDetail)
-
-	//request log
-	v1.GET("/log/operations", logController.OperationLogLists)
-	v1.GET("/log/operations/:id", logController.OperationLogDetail)
-
-	// Ldap & email setting
-	settingController := controllers.SettingController{}
-	v1.GET("/setting/ldap", settingController.LdapList)
-	v1.POST("/setting/ldap", settingController.LdapUpdate)
-	v1.GET("/setting/email", settingController.EmailList)
-	v1.POST("/setting/email", settingController.EmailUpdate)
-	v1.GET("/setting/sync/dingtalk", settingController.SyncDingtalk)
-
-	// Sync actions
-	syncController := controllers.SyncController{}
-	v1.POST("/sync/dingtalk", syncController.SyncDingTalk)
 
 	if viper.GetBool("project.merge") {
 		e.LoadHTMLGlob("./pkg/webui/dist/*.html") // 添加入口index.html
@@ -168,25 +36,4 @@ func SetUp(e *gin.Engine, cors bool) {
 		e.Static("/admin/static", "./pkg/webui/dist/static")   // 添加资源路径
 		e.StaticFile("/admin/", "./pkg/webui/dist/index.html") //前端接口
 	}
-
-	// admin-service
-	AmsController := &controllers.AmsController{}
-	v1.POST("/amstech", AmsController.Amstech_rpc)
-	v1.POST("/amstechevent", AmsController.Amstech_rpcevent)
-	v1.POST("/uploadImage", AmsController.Amstech_upload)
-	v1.POST("/uploadAPK", AmsController.Amstech_apk)
-	v1.POST("/removeAPK", AmsController.Amstech_removeapk)
-	v1.POST("/removeFile", AmsController.Amstech_remove)
-	v1.POST("/uploadpemfile", AmsController.Amstech_pemfile)
-	v1.POST("/removepemfile", AmsController.Amstech_removepemfile)
-	v1.POST("/log/amslogs", AmsController.AMSlogs)
-	v1.POST("/uploaddeliverynotes", AmsController.Amstech_deliverynotes)
-	v1.POST("/removedeliverynotes", AmsController.Amstech_removedeliverynotes)
-	v1.POST("/uploadrefundfile", AmsController.Amstech_uploadrefundfile)
-	v1.POST("/removerefundfile", AmsController.Amstech_removerefundfile)
-	// admin-ERP-service
-	ERPController := &controllers.ERPController{}
-	// v1.POST("/erpParser", ERPController.erpParserfunc)
-	v1.POST("/axiomerp/get", ERPController.GetPO)
-	v1.POST("/axiomerp/update", ERPController.UpdatePO)
 }
